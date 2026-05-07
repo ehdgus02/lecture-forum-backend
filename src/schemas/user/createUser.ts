9b49f6933a7198c9c15a67ba28b451c00510d8e2
ehdgus02 <ehdgus02kk@naver.com>
2026-05-07 16:28:27 +0900

    validate middleware 추가

--- src/schemas/user/createUser.ts
+++ src/schemas/user/createUser.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 import { GenderType } from "../../generated/prisma/enums.ts";
 
-// zod를 통해 검증할 Input 값에 객체모양 생성
-const createUserSchema = z.object({
+// zod를 통해 검증할 Input값에 대한 객체모양 생성
+export const createUserSchema = z.object({
     username: z.string().min(4),
     password: z.string().min(6),
     name: z.string().min(2),
-    nickname: z.string().min(2).max(50),
-    email: z.string(),
+    nickname: z.string().min(2).max(10),
+    email: z.email(),
     phoneNumber: z.string().optional(),
     birthdate: z.iso.datetime().optional(),
     gender: z.enum(GenderType),
