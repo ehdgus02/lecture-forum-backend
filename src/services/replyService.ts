d5e60bd77e8e74868b44c56362c61f5213e524cc
ehdgus02 <ehdgus02kk@naver.com>
2026-06-02 17:13:10 +0900

    replyController.getRepliesByPostId 작성 완료

--- src/services/replyService.ts
+++ src/services/replyService.ts
@@ -1,5 +1,39 @@
 import prisma from "../config/prisma.ts";
 
+const getRepliesByPostId = async (postId: number, page: number, size: number) => {
+    // 목록을 불러오는 목적의 service니까
+    // pagination도 해야되는구나 => skip, take를 써야됨
+    const skip = (page - 1) * size;
+
+    const total = await prisma.reply.count({
+        where: { postId },
+    });
+
+    const list = await prisma.reply.findMany({
+        where: {
+            postId,
+        },
+        take: size,
+        skip,
+        orderBy: { id: "desc" },
+        include: {
+            user: {
+                select: {
+                    id: true,
+                    nickname: true,
+                },
+            },
+        },
+    });
+
+    return {
+        page,
+        size,
+        total,
+        list,
+    };
+};
+
 const createReply = async (userId: number, postId: number, content: string) => {
     // 이 댓글이 달릴 글이 살아있는 글인가를 체크를 먼저 함
     // 그러면 왜 userId 살아있는 사용자는 체크 안하나요?
@@ -35,5 +69,6 @@ const createReply = async (userId: number, postId: number, content: string) => {
 };
 
 export default {
+    getRepliesByPostId,
     createReply,
 };
