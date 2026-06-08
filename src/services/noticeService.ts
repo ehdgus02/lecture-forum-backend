fcc346ab0afee6f5755bb2bae199f699064ea1ec
ehdgus02 <ehdgus02kk@naver.com>
2026-06-08 11:46:18 +0900

    noticeController 작성 완료

--- src/services/noticeService.ts
+++ src/services/noticeService.ts
@@ -12,6 +12,26 @@ const getNoticeById = async (id: number) => {
     return notice;
 };
 
+const getNoticeList = async (page: number, size: number) => {
+    // prisma에게 페이지네이션을 하기 위해
+    // skip과 take를 전달해줘야 하는데
+    // take는 말 그대로 가져와야 되는 갯수를 뜻하고
+    // skip은 데이터를 지나치는 갯수를 뜻함
+    // (내가 3페이지를 보고 싶으니, 30개 데이터 이후의 15개를 가져와라)
+
+    const list = prisma.notice.findMany({
+        orderBy: { id: "desc" },
+        skip: (page - 1) * size,
+        take: size,
+    });
+    const total = await prisma.notice.count();
+
+    return {
+        total,
+        list,
+    };
+};
+
 const createNotice = async (title: string, content: string) => {
     return prisma.notice.create({
         data: {
@@ -51,6 +71,7 @@ const deleteNotice = async (id: number) => {
 
 export default {
     getNoticeById,
+    getNoticeList,
     createNotice,
     updateNotice,
     deleteNotice,
